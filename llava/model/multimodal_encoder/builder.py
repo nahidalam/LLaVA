7fa8273dbbb1d4e42f6020dc2bdb42deb1d282a6
Nahid Alam <shapla@gmail.com>
2024-07-12 21:53:46 +0000

    builder update for siglip

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -9,9 +9,9 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     use_s2 = getattr(vision_tower_cfg, 's2', False)
     if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
         if use_s2:
-            #return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
-            return SiglipVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+            return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
         else:
-            #return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-            return SiglipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    elif 'siglip' in vision_tower:
+        return SiglipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     raise ValueError(f'Unknown vision tower: {vision_tower}')
