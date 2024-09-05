8ba6a932e513c06cd7537b2ca70faf7ff7de3c40
Nahid Alam <shapla@gmail.com>
2024-09-05 16:34:48 +0000

    batch size 16 for instruction tune

--- scripts/cohere/finetune_lora.sh
+++ scripts/cohere/finetune_lora.sh
@@ -20,7 +20,7 @@ deepspeed llava/train/train_mem.py \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
