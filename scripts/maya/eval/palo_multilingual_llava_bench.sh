d8f0b97696d4bb2ed97189fe489723713b59958d
Nahid Alam <shapla@gmail.com>
2024-10-23 05:38:40 +0000

    all lang eval

--- scripts/maya/eval/palo_multilingual_llava_bench.sh
+++ scripts/maya/eval/palo_multilingual_llava_bench.sh
@@ -74,28 +74,28 @@ evaluate_language() {
 evaluate_language "English" "$MULTILINGUAL_LLAVA_BENCH_PATH/english/questions.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/english/answers_gpt4.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/english/context.jsonl"
 
 # 2.Chinese
-# evaluate_language "Chinese" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/context.jsonl"
+evaluate_language "Chinese" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/chinese/context.jsonl"
 
 # 3.Spanish
-# evaluate_language "Spanish" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/context_spanish.jsonl"
+evaluate_language "Spanish" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/spanish/context_spanish.jsonl"
 
 # 4.French
-# evaluate_language "French" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/context.jsonl"
+evaluate_language "French" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/french/context.jsonl"
 
 # 5.Russian
-# evaluate_language "Russian" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/context.jsonl"
+evaluate_language "Russian" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/russian/context.jsonl"
 
 # 6.Arabic
-# evaluate_language "Arabic" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/context.jsonl"
+evaluate_language "Arabic" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/arabic/context.jsonl"
 
 # 7.Bengali -- Not currently supported
 # evaluate_language "Bengali" "$MULTILINGUAL_LLAVA_BENCH_PATH/bengali/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/bengali/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/bengali/context.jsonl"
 
 # 8.Hindi
-# evaluate_language "Hindi" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/context.jsonl"
+evaluate_language "Hindi" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/hindi/context.jsonl"
 
 # 9.Urdu -- Not currently supported
 # evaluate_language "Urdu" "$MULTILINGUAL_LLAVA_BENCH_PATH/urdu/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/urdu/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/urdu/context.jsonl"
 
 # 10.Japanese
-# evaluate_language "Japanese" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/context.jsonl"
+evaluate_language "Japanese" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/question.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/answers.jsonl" "$MULTILINGUAL_LLAVA_BENCH_PATH/japanese/context.jsonl"
