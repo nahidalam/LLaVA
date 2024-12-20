2b00ab2001469616f96f9cbc32b288304e67a678
nahalam <shapla@gmail.com>
2024-12-19 21:38:52 -0800

    aimv2 script and training code

--- llava/train/train.py
+++ llava/train/train.py
@@ -777,6 +777,8 @@ class LazySupervisedDataset(Dataset):
             #crop_size = self.data_args.image_processor.crop_size or {'height': 224, 'width': 224}
             if 'siglip' in self.data_args.image_processor.image_processor_type.lower():
                 crop_size = {'height': 256, 'width': 256}
+            elif 'aimv2' in self.data_args.image_processor.image_processor_type.lower():
+                crop_size = {'height': 224, 'width': 224}
             else:
                 crop_size = self.data_args.image_processor.crop_size
             data_dict['image'] = torch.zeros(3, crop_size['height'], crop_size['width'])
--- scripts/v1_5/pretrain_aya_aimv2.sh
+++ scripts/v1_5/pretrain_aya_aimv2.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+
+MODEL_VERSION=aya-23-8b-siglip-multi
+
+########### DO NOT CHANGE ###########
+########### USE THIS FOR BOTH ###########
+PROMPT_VERSION=plain
+########### DO NOT CHANGE ###########
+
 deepspeed llava/train/train_mem.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path CohereForAI/aya-23-8B \
-    --version aya \
-    --data_path /dev/data/annotations/ \
-    --image_folder /dev/data/instruction_tune_dataset/ \
-    --vision_tower google/siglip-base-patch16-256-multilingual \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-aya-23-8b-siglip-multi-8lang-32bz-pretrain/mm_projector.bin \
+    --version $PROMPT_VERSION \
+    --data_path /dev/data/LLaVA_Pretrain \
+    --image_folder /dev/data/images \
+    --vision_tower apple/aimv2-large-patch14-224 \
     --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/maya \
+    --output_dir ./checkpoints/llava-$MODEL_VERSION-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 24000 \
     --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --learning_rate 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
