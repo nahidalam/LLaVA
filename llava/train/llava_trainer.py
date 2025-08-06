31eca546ce473da8bb454b47a36f30f331f22ba6
Nahid Alam <shapla@gmail.com>
2025-08-06 01:51:34 +0000

    update

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -132,7 +132,7 @@ class LengthGroupedSampler(Sampler):
 
 class LLaVATrainer(Trainer):
 
-    def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
+    def _get_train_sampler(self, dataset) -> Optional[torch.utils.data.Sampler]:
         if self.train_dataset is None or not has_length(self.train_dataset):
             return None
 
