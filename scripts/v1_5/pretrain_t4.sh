c2a6166b621d76b03e56b2d576d9c641a694ab2a
pecies <patboss63@gmail.com>
2025-08-05 23:38:58 -0700

    change back original files

--- scripts/v1_5/pretrain_t4.sh
+++ scripts/v1_5/pretrain_t4.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 deepspeed llava/train/train_sdpa.py \
-    --deepspeed ./scripts/zero3.json \
+    --deepspeed ./scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
-    --version v1 \
-    --data_path /dev/data/llava_annotation/llava_v1_5_mix665k.json \
-    --image_folder /dev/data/instruction_tune_dataset \
+    --version plain \
+    --data_path /kaggle/working/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder /kaggle/input/llava-pretrainimages-zip \
     --vision_tower google/siglip-base-patch16-256 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain-siglip-base-patch16-256/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-finetune-siglip \
+    --output_dir ./checkpoints/llava-v1.5-7b-pretrain-siglip-base-patch16-256 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 5000 \
     --save_total_limit 1 \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -32,6 +30,6 @@ deepspeed llava/train/train_sdpa.py \
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 2 \
     --lazy_preprocess True \
     --report_to wandb
