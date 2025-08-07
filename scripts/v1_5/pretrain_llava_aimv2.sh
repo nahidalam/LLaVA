412858d6a994f2187538fcc309b8af37b380255f
Wesley <producerww@gmail.com>
2025-08-07 14:27:42 -0700

    change gradient accumulation steps

--- scripts/v1_5/pretrain_llava_aimv2.sh
+++ scripts/v1_5/pretrain_llava_aimv2.sh
@@ -17,7 +17,7 @@ deepspeed llava/train/train_mem.py \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
